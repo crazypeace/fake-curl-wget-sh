55347174f1db2f6920a91899900d415ea3acd69b
crazypeace <txican@gmail.com>
2025-11-12 06:17:38 +0800

    Update fake-curl-wget-v2.sh

--- fake-curl-wget-v2.sh
+++ fake-curl-wget-v2.sh
@@ -3,13 +3,13 @@
 # 拦截 curl 和 wget 调用，用于调试/离线测试
 # ==========================================
 
-# 移除fake路径
+# 移除PATH中的fake路径
 export PATH=$(echo "$PATH" | sed 's|^\(/tmp/fakebin:\)\+||')
 
-# 确保目录存在
+# 确保fake目录存在
 mkdir -p /tmp/fakebin
 
-# 清理目录
+# 清理fake目录
 rm /tmp/fakebin/*
 
 # 用 real-curl 来调用真实的系统curl
@@ -19,15 +19,16 @@ chmod +x /tmp/fakebin/real-curl
 echo "$(which wget) \"\$@\"" > /tmp/fakebin/real-wget
 chmod +x /tmp/fakebin/real-wget
 
+# 用 假的 curl wget 来拦截网络访问
 # 放入假命令文件
 cp __fake_net_common.sh /tmp/fakebin/fake_net_common
-chmod +x $_
-cp __fake_curl.sh /tmp/fakebin/curl
-chmod +x $_
-cp __fake_wget.sh /tmp/fakebin/wget
-chmod +x $_
+chmod +x /tmp/fakebin/fake_net_common
+echo "fake_net_common curl \"\$@\"" > /tmp/fakebin/curl
+chmod +x /tmp/fakebin/curl
+echo "fake_net_common wget \"\$@\"" > /tmp/fakebin/wget
+chmod +x /tmp/fakebin/wget
 
-# 在PATH中设置假命令 curl wget 的目录
+# 在PATH中添加fake路径
 if [[ ":$PATH:" != *:/tmp/fakebin:* ]]; then
   export PATH="/tmp/fakebin:$PATH"
 fi
